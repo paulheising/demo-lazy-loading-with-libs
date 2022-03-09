8b491d71d6bfe6d4d73bb2e54228182a2b482b83
Paul Heising <paul.heising@wetteronline.de>
2022-03-09 20:20:55 +0100

    add hints

--- projects/foo-app/src/app/app.module.ts
+++ projects/foo-app/src/app/app.module.ts
@@ -8,7 +8,14 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AlphaModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // Remove this line and the usage in app.component.html to see lib
+    // references moving to lazy bundle. Add it again and all lib references go
+    // to main bundle (don't forget to add/remove the import above)
+    AlphaModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
